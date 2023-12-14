6d4a8a3dfc136a076bdb7d0164e8b9713399bc4c
lmatuzic <luka.matuzic1@gmail.com>
2023-12-14 17:39:55 +0100

    add loading spinner

--- src/features/tasks/components/task-management-board/TaskManagementBoard.tsx
+++ src/features/tasks/components/task-management-board/TaskManagementBoard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import PrimaryButton from '../../../../components/button/PrimaryButton';
+import LoadingSpinner from '../../../../components/loading/LoadingSpinner';
 import useTaskContext from '../../context/useTaskContext';
 import { useTaskFilter } from '../../hooks/useTaskFilter';
 import useTeamMembers from '../../hooks/useTeamMembers';
@@ -36,47 +37,49 @@ export default function TaskManagementBoard() {
         fetchTeamMembers();
     }, [fetchTeamMembers]);
 
-    if (isFetchingTeamMembers) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <div className='task-management-board'>
-            <header>
-                <div className='add-new-task'>
-                    <input
-                        type='text'
-                        name='todo-name'
-                        value={taskName}
-                        placeholder='Type task name'
-                        required
-                        onChange={(e) => handleSetTaskName(e.target.value)}
+            {isFetchingTeamMembers ? (
+                <LoadingSpinner />
+            ) : (
+                <>
+                    <header>
+                        <div className='add-new-task'>
+                            <input
+                                type='text'
+                                name='todo-name'
+                                value={taskName}
+                                placeholder='Type task name'
+                                required
+                                onChange={(e) => handleSetTaskName(e.target.value)}
+                            />
+
+                            <PrimaryButton
+                                onClick={handleAddTask}
+                                type='submit'
+                            >
+                                Add Task
+                            </PrimaryButton>
+                        </div>
+
+                        {teamMembers && (
+                            <TaskFilter
+                                handleSetSelectedTeamMember={handleSetSelectedTeamMember}
+                                selectedTeamMember={selectedTeamMember}
+                                handleSetSelectedDueDate={handleSetSelectedDueDate}
+                                selectedDueDate={selectedDueDate}
+                                handleSetSelectedPriority={handleSetSelectedPriority}
+                                selectedPriority={selectedPriority}
+                            />
+                        )}
+                    </header>
+
+                    <Kanban
+                        tasks={filteredTasks}
+                        draggedTask={draggedTask}
                     />
-
-                    <PrimaryButton
-                        onClick={handleAddTask}
-                        type='submit'
-                    >
-                        Add Task
-                    </PrimaryButton>
-                </div>
-
-                {teamMembers && (
-                    <TaskFilter
-                        handleSetSelectedTeamMember={handleSetSelectedTeamMember}
-                        selectedTeamMember={selectedTeamMember}
-                        handleSetSelectedDueDate={handleSetSelectedDueDate}
-                        selectedDueDate={selectedDueDate}
-                        handleSetSelectedPriority={handleSetSelectedPriority}
-                        selectedPriority={selectedPriority}
-                    />
-                )}
-            </header>
-
-            <Kanban
-                tasks={filteredTasks}
-                draggedTask={draggedTask}
-            />
+                </>
+            )}
         </div>
     );
 }
